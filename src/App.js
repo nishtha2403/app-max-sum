48158107f4eb7d4989a339e65fd387f6020b3c7b
Nishtha Sharma <nishtha@Nishthas-MacBook-Air-2.local>
2023-01-04 18:05:01 +0530

    App initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [enteredExpression, setEnteredExpression] = useState();
+  const [expressionArray, setExpressionArray] = useState([]);
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [maxSum, setMaxSum] = useState(0);
+  
+  const onSubmit = () => {
+    const expressionArray = enteredExpression.split(',');
+    const sortedArray = expressionArray.sort();
+    const num1 = parseInt(sortedArray[sortedArray.length - 1]) || 0;
+    const num2 = parseInt(sortedArray[sortedArray.length - 2]) || 0;
+    setExpressionArray(expressionArray);
+    setNum1(num1);
+    setNum2(num2); 
+    setMaxSum(num1 + num2);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='leftPanel'>
+      {
+            expressionArray.length > 0 && 
+            <div className='container'>
+              {
+                expressionArray.map(exp => <p>{exp}</p>)
+              }
+            </div>
+      }
+        
+      </div>
+      <div className='rightPanel'>
+        <div className='inputPanel'>
+          <input 
+            type="text"
+            value={enteredExpression}
+            onChange={(e) => {
+              setEnteredExpression(e.target.value);
+            }}
+          />
+          <button onClick={onSubmit}>Submit</button>
+        </div>
+        <div className='outputPanel'>
+          {
+            expressionArray.length > 0 && <h4>Maximum sum of two numbers ({num1}, {num2}) are {maxSum}</h4>
+          }
+        </div>
+      </div>
     </div>
   );
 }
